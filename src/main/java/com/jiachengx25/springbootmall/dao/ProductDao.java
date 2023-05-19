2a0c8a6b1df0589a20e49c85d9bc4406f1cdfc38
jiachengx25 <jiachengx25@gmail.com>
2023-05-20 00:00:33 +0800

    商品功能 實作查詢商品列表 (基礎實作)

--- src/main/java/com/jiachengx25/springbootmall/dao/ProductDao.java
+++ src/main/java/com/jiachengx25/springbootmall/dao/ProductDao.java
@@ -3,7 +3,12 @@ package com.jiachengx25.springbootmall.dao;
 import com.jiachengx25.springbootmall.dto.ProductRequest;
 import com.jiachengx25.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/jiachengx25/springbootmall/service/ProductService.java
+++ src/main/java/com/jiachengx25/springbootmall/service/ProductService.java
@@ -3,7 +3,12 @@ package com.jiachengx25.springbootmall.service;
 import com.jiachengx25.springbootmall.dto.ProductRequest;
 import com.jiachengx25.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
